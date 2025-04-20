288ba58da5e09c93d1b519f4f5a1a5e0d45f59d7
manmadha89 <madhangopalakrishna@gmail.com>
2025-04-20 08:25:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("basicForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Basic Plan Selected!");
+  });
+
+  
+  document.getElementById("premiumForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Premium Plan Selected!");
+  });
+
+  document.getElementById("luxuryForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Luxury Plan Selected!");
+  });
+  
